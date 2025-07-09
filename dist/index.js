a861e6659a253ceafac8e1f8bc3ad02ea21d36dc
Cícero Feijó <cicero.feijo@gmail.com>
2025-07-08 22:27:12 -0300

    feat: adiciona primeira rota raiz

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
-console.log('Funciona mesmo!');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const porta = 3030;
+app.get('/', (req, res) => {
+    res.send('Executando a NODE JS API REST');
+});
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Funciona mesmo!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const porta = 3030;
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Executando a NODE JS API REST');
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}`);
+})
